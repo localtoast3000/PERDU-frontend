dda9c51c05ccc19553216a1c45654e9555ec9641
UPME User <User.UPME@hpi-eu.com>
2022-12-07 16:24:29 +0100

    Index page start

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+  <main class="main-container">
+    <header class="header-container">
+      <Logo class="logo" />
+      <h1 class="header"><span class="header-top">THINK YOUR LOST ITEMS ARE GONE FOR GOOD?</span><span class="header-bottom">WE SAY NO WAY!</span></h1>
+    </header>
+    <div></div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'Index'
 }
 </script>
+
+<style scoped>
+
+</style>
