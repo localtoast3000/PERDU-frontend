78c0d6e777c10c7fd3df6ae636e95fe89ff81dbb
MRcraigchick <craigchick@outlook.com>
2022-12-08 06:32:56 +0100

    Logo styles

--- components/Button.vue
+++ components/Button.vue
@@ -1,23 +1,18 @@
 <template>
-    <button class="btn" @click="handleClick">{{label}}</button>
+  <button class="btn" @click="handleClick">{{ label }}</button>
 </template>
 
 <script>
-    export default {
-        props: {
-            label: String,
-        },
-        methods: {
-            handleClick(e){
-                console.log(e)
-            }
-        }
-    }
+export default {
+  props: {
+    label: String,
+  },
+  methods: {
+    handleClick(e) {
+      console.log(e);
+    },
+  },
+};
 </script>
 
-<style scoped>
-    .btn {
-        background-color: red;
-        border: 1px solid black;
-    }
-</style>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,20 @@
+<script>
+export default {
+  name: "Index",
+};
+</script>
+
+<style scoped></style>
+
 <template>
   <main class="main-container">
     <header class="header-container">
-      <Logo class="logo" />
-      <h1 class="header"><span class="header-top">THINK YOUR LOST ITEMS ARE GONE FOR GOOD?</span><span class="header-bottom">WE SAY NO WAY!</span></h1>
+      <SvgLogo className="logo" :scale="2" color="var(--primary)" />
+      <h1 class="header">
+        <span class="header-top">THINK YOUR LOST ITEMS ARE GONE FOR GOOD?</span
+        ><span class="header-bottom">WE SAY NO WAY!</span>
+      </h1>
     </header>
     <div></div>
   </main>
 </template>
-
-<script>
-export default {
-  name: 'Index'
-}
-</script>
-
-<style scoped>
-
-</style>
